0221a43eb169d3fe0a5633754bef928263694277
Liberhome <46431509+Liberhome@users.noreply.github.com>
2020-03-06 22:25:11 +0800

    Update 206_2.java

--- 206_2.java
+++ 206_2.java
@@ -1,6 +1,10 @@
+
+
+//
 public ListNode reverseList(ListNode head) {
     ListNode newHead = new ListNode(-1);
     while (head != null) {
+        
         ListNode next = head.next;
         head.next = newHead.next;
         newHead.next = head;
